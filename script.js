fce0c0794d475a7d771ff65018bd3293437a17f7
AmidokS <aalimhul@gmail.com>
2025-05-07 05:40:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const gallery = document.getElementById("gallery");
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.getElementById("gallery");
 
-for (let i = 1; i <= 20; i++) {
-    const link = document.createElement("a");
-    link.href = `image/kuchnia${i}.jpg`;
-    link.setAttribute("data-lightbox", "meble");
-    link.setAttribute("data-title", `Kuchnia ${i}`);
+    for (let i = 1; i <= 20; i++) {
+        const link = document.createElement("a");
+        link.href = `image/kuchnia${i}.jpg`;
+        link.setAttribute("data-lightbox", "meble");
+        link.setAttribute("data-title", `Kuchnia ${i}`);
 
-    const img = document.createElement("img");
-    img.src = `image/kuchnia${i}.jpg`;
-    img.alt = `kuchnia${i}`;
+        const img = document.createElement("img");
+        img.src = `image/kuchnia${i}.jpg`;
+        img.alt = `kuchnia${i}`;
 
-    link.appendChild(img);
-    gallery.appendChild(link);
-}
+        link.appendChild(img);
+        gallery.appendChild(link);
+    }
 
-const burger = document.getElementById("burger");
-const nav = document.querySelector("nav");
+    const burger = document.getElementById("burger");
+    const nav = document.querySelector("nav");
 
-burger.addEventListener("click", () => {
-    nav.classList.toggle("active");
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
 });
